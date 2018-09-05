309a368efcd3d996cce59769c9f52df4181b4b6a
jponly <jpangeli514@gmail.com>
2018-09-04 21:44:04 -0700

    added html

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// require("")(app);
-// require("")(app);
+ require("./app/routing/apiRoutes")(app);
+ require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("app are listening on " + PORT);
